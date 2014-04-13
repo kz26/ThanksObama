f19808c24ec740ad4ebcb09b4f13c648306a4338
whitehat2k9 <whitehat2k9@gmail.com>
2014-04-12 23:19:09 -0500

    Backend mostly done

--- thanksobama/main/imo_api.py
+++ thanksobama/main/imo_api.py
@@ -8,12 +8,12 @@ from time import sleep
 class IMOInterface(object):
 	def __init__(self):
 		self.sock = socket.create_connection(('66.252.70.188', 42011))
-		self.sock.settimeout(2)
+		self.sock.setblocking(1)
 		self.orgID = 'd03b26e60936db9f'
 		self.expiry_workaround = True
 
-	def get_imo_from_icd10(self, icd10_code):
-		self.sock.send("search^1|0|2|1^%s^%s\n" % (icd10_code, self.orgID))
+	def get_imo_from_query(self, query):
+		self.sock.send("search^1|0|2|1^%s^%s\n" % (query, self.orgID))
 		sleep(0.25)
 		L = struct.unpack('!I', self.sock.recv(4))[0]
 		data = self.sock.recv(L)
@@ -21,6 +21,8 @@ class IMOInterface(object):
 		if self.expiry_workaround and len(data['data']['items']):
 			data['data']['items'] = data['data']['items'][1:]
 		if len(data['data']['items']):
-			return data['data']['items'][0]['title']
+			x = data['data']['items'][0]
+			return x['title']
 		else:
 			return None
+
--- thanksobama/main/lcd_db.py
+++ thanksobama/main/lcd_db.py
@@ -3,6 +3,7 @@ import sqlite3
 class LCDInterface(object):
 	def __init__(self):
 		self.conn = sqlite3.connect('lcd.sqlite')
+		self.conn.text_factory = lambda x: unicode(x, 'utf-8', 'ignore') # fix unicode issues
 
 	def random_icd10(self):
 		# return a random ICD10 code from the ICD10 supported procedures table
@@ -10,17 +11,33 @@ class LCDInterface(object):
 		return qr.fetchone()[0]
 
 	def random_procedure(self):
-		# return a random procedure id, title from the LCD table
-		qr = self.conn.execute("SELECT DISTINCT * FROM lcd ORDER BY RANDOM() LIMIT 1")
+		# return a random procedure from the LCD table
+		qr = self.conn.execute("SELECT DISTINCT title FROM lcd ORDER BY RANDOM() LIMIT 1")
+		return qr.fetchone()
+
+	def random_supported_procedure(self):
+		# return a random supported procedure that appears IN the icd10_support table
+		qr = self.conn.execute("SELECT * FROM lcd WHERE lcd_id IN (SELECT DISTINCT lcd_id FROM icd10_support ORDER BY RANDOM() LIMIT 1)")
 		return qr.fetchone()
 
 	def get_icd10_procedures(self, icd10):
 		# return supported procedures for a given ICD10
-		qr = self.conn.execute("SELECT title FROM lcd WHERE lcd_id in (SELECT lcd_id FROM icd10_support WHERE icd10=?", (icd10,))
+		qr = self.conn.execute("SELECT title FROM lcd WHERE lcd_id IN (SELECT lcd_id FROM icd10_support WHERE icd10=?)", (icd10,))
 		return qr.fetchall()
 
 	def get_icd10_unsupported_procedures(self, icd10):
 		# return unsupported procedures for a given ICD10
-		qr = self.conn.execute("SELECT title FROM lcd WHERE lcd_id in (SELECT lcd_id FROM icd10_nosupport WHERE icd10=?", (icd10,))
+		qr = self.conn.execute("SELECT title FROM lcd WHERE lcd_id IN (SELECT lcd_id FROM icd10_nosupport WHERE icd10=?)", (icd10,))
 		return qr.fetchall()
 
+	def get_procedure_supported_icd10s(self, lcd_id):
+		# return supported ICD10s associated with a procedure ID
+		qr = self.conn.execute("SELECT icd10 FROM icd10_support WHERE lcd_id=?", (lcd_id,))
+		return qr.fetchall()
+
+	def get_procedure_unsupported_icd10s(self, lcd_id):
+		# return unsupported ICD10s associated with a procedure ID
+		qr = self.conn.execute("SELECT icd10 FROM icd10_nosupport WHERE lcd_id=?", (lcd_id,))
+		return qr.fetchall()
+
+LCDInterface = LCDInterface()
